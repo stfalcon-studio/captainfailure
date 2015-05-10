1217db3139d4b095e55e9f8e3bf2aa953a6aa98a
Evgeniy Gurinovich <jeka@stfalcon.com>
2015-05-10 19:14:27 +0300

    ability to managing users

--- app/controllers/manageusers_controller.rb
+++ app/controllers/manageusers_controller.rb
@@ -1,7 +1,62 @@
 class ManageusersController < ApplicationController
+
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+  #get /users/manage
   def index
     @active = 'users'
     @users = User.all
     render 'manageusers/index'
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+    p @user
+  end
+
+  #post /users/manage
+  def create
+    @user = User.create(params.require(:user).permit(:name, :email, :password, :password_confirmation))
+    if @user.errors.empty?
+      redirect_to '/users/manage'
+    else
+      render 'edit'
+    end
+  end
+
+  #get /users/manage/:id
+  def edit
+  end
+
+  #put /users/manage/:id
+  def update
+    if params[:user][:password] == '' and params[:user][:password_confirmation] == ''
+      update_params = :name, :email
+    else
+      update_params = :name, :email, :password, :password_confirmation
+    end
+    p params[:user]
+    @user.update_attributes(params.require(:user).permit(update_params))
+    if @user.errors.empty?
+      redirect_to '/users/manage'
+    else
+      p @user.errors
+      render 'edit'
+    end
+  end
+
+  #delete /users/manage/:id
+  def destroy
+    @user.destroy
+    redirect_to action: 'index'
+  end
+
+  private
+
+  def find_user
+    @user = User.where('id = ?', params[:id]).first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'dashboard#index'
-  get 'users/manage' => 'manageusers#index'
+  # get 'users/manage'     => 'manageusers#index'
+  # get 'users/manage/:id' => 'manageusers#edit', :as => :user
+  # patch 'users/manage/:id' => 'manageusers#update'
+  resources :manageusers, path: '/users/manage', as: :user
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
