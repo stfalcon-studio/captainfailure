308fc03974697b60445c876d3464405af2901585
Evgeniy Gurinovich <jeka@stfalcon.com>
2015-07-30 17:10:45 +0300

    add filter by server for activity

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -13,14 +13,25 @@
 # limitations under the License.
 
 class DashboardController < ApplicationController
+  before_action :find_all_servers, :select_server
+
   def index
-    @activity = CheckResult.paginate(:page => params[:page], :per_page => 30).order('id DESC')
+    @activity = CheckResult.all
+    unless @selected_server == 'all'
+      server_id = Server.where('dns_name = ?', @selected_server)
+      @activity = @activity.where(server_id: server_id)
+    end
+    @activity = @activity.paginate(:page => params[:page], :per_page => 30).order('id DESC')
     @active = 'all'
     render 'index'
   end
 
   def failed
     @activity = CheckResult.where(passed: false)
+    unless @selected_server == 'all'
+      server_id = Server.where('dns_name = ?', @selected_server)
+      @activity = @activity.where(server_id: server_id)
+    end
     @activity = @activity.paginate(:page => params[:page], :per_page => 30).order('id DESC')
     @active = 'failed'
     render 'index'
@@ -28,9 +39,33 @@ class DashboardController < ApplicationController
 
   def passed
     @activity = CheckResult.where(passed: true)
+    unless @selected_server == 'all'
+      server_id = Server.where('dns_name = ?', @selected_server)
+      @activity = @activity.where(server_id: server_id)
+    end
     @activity = @activity.paginate(:page => params[:page], :per_page => 30).order('id DESC')
     @active = 'passed'
     render 'index'
+  end
+
+  private
+  def find_all_servers
+    @servers = Server.all
+  end
+
+  def select_server
+    if params[:server]
+      @selected_server = params[:server]
+      render text: 'Not found', status: 404 unless server_names.include?(@selected_server) or @selected_server == 'all'
+    else
+      @selected_server = 'all'
+    end
+  end
+
+  def server_names
+    server_names = []
+    @servers.each { |server| server_names << server.dns_name }
+    server_names
   end
 
 end
