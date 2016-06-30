248f256425ef9e8d6913eb110f1ff261e0cea57c
Jeka <jeka@stfalcon.com>
2016-06-30 15:27:56 +0300

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :log_level, :info
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/application.rb', 'config/initializers/devise.rb', 'config/database.yml', 'config/secrets.yml', 'config/unicorn.rb')
+set :linked_files, fetch(:linked_files, []).push('config/application.rb', 'config/environment.rb', 'config/initializers/devise.rb', 'config/database.yml', 'config/secrets.yml', 'config/unicorn.rb')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
