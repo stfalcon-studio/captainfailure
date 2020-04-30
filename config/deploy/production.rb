f0e6e72624d0ac36f236fb12b7051c977aa55d71
Yevhen Gurinovich <jeka@stfalcon.com>
2020-04-30 11:20:51 +0300

    dynamic min value in aviability visualization

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
-server '144.76.238.50', user: 'captainfailure', roles: %w{app db web}
+server '159.69.196.71', user: 'captainfailure', roles: %w{app db web}
 
 
 # role-based syntax
